d39498f4978a0e95ea40a114a205e0fcdad2e9b1
bambara <h.bambara.1025@gmail.com>
2020-02-26 06:49:50 +0900

    rescratch App.js

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,18 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  
+  constructor(){
+    super()
+    this.state = {
+      todo:[
+        {
+          id: 1,
+          title: "Hello, React",
+          desc: "React始めました",
+          done: false
+        },
+        {
+          id: 2,
+          title: "Hello, Redux!",
+          desc: "Reduxも始めました",
+          done: false
+        },
+      ]
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h1>todoアプリを作ってみた</h1>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div>
+          <todoList
+            todos={this.state.todos}
+          />
+        </div>
       </div>
     );
   }
